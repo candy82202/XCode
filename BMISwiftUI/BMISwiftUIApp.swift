9afa5da45bc904f148030560cf584b428f64824a
candy82202 <candy82202@yahoo.com.tw>
2022-08-06 12:47:48 +0800

    Initial Commit

---
+++ BMISwiftUI/BMISwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMISwiftUIApp.swift
+//  BMISwiftUI
+//
+//  Created by Apple on 2022/8/6.
+//
+
+import SwiftUI
+
+@main
+struct BMISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
